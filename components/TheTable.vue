1929ead32cfd730fbe48a6961e2f591a33b62bf2
Maksim <maxim4344511@gmail.com>
2022-10-17 18:26:51 +0300

    Изменил перебор массива циклом для пагинации, на метод slice.

--- components/TheTable.vue
+++ components/TheTable.vue
@@ -3,15 +3,15 @@
 		<div class="filter">
 			<div>
 				<select v-model="param">
-					<option value=" ">Параметр</option>
+					<option value="">Параметр</option>
 					<option value="name" >Название</option>
-					<option value="quontity">Количество</option>
+					<option value="amount">Количество</option>
 					<option value="distance">Расстояние</option>
 				</select>
 			</div>
 			<div>
 				<select v-model="op" placeholder="Значение">
-					<option value=" ">Условие</option>
+					<option value="">Условие</option>
 					<option value=">">Больше</option>
 					<option value="<">Меньше</option>
 					<option value="=">Равно</option>
@@ -35,12 +35,12 @@
 				<div class="table__rec" v-for="(rec, index) in part" :key="index">
 					<div>{{ rec.date }}</div>
 					<div>{{ rec.name }}</div>
-					<div>{{ rec.quontity }}</div>
+					<div>{{ rec.amount }}</div>
 					<div>{{ rec.distance }}</div>
 				</div>
 			</div>
 		</div>
-		<button @click="back">назад</button> {{ pagin / 10 + 1 }} <button @click="next">вперед</button>
+		<button @click="back">назад</button> {{ offset / 10 + 1 }} <button @click="next">вперед</button>
 	</div>
 </template>
 
@@ -49,43 +49,44 @@
 export default {
 	data() {
 		return {
-			pagin: 0,
-			valueToFilter: ' ',
-			param: ' ',
-			op: ' ',
+			offset: 0,
+			valueToFilter: '',
+			param: '',
+			op: '',
 			res: false,
 			resultFilter: [],
 			schedule: [
-				{date: '1.01.2022', name: 'AA', quontity: 8, distance: 6000},
-				{date: '2.01.2022', name: 'aa', quontity: 6, distance: 5000},
-				{date: '3.01.2022', name: 'Ab', quontity: 2, distance: 8000},
-				{date: '4.01.2022', name: 'ab', quontity: 3, distance: 3400},
-				{date: '5.01.2022', name: 'BB', quontity: 4, distance: 4500},
-				{date: '6.01.2022', name: 'bb', quontity: 8, distance: 6000},
-				{date: '7.01.2022', name: 'bA', quontity: 4, distance: 4500},
-				{date: '8.01.2022', name: 'cc', quontity: 1, distance: 6220},
-				{date: '9.01.2022', name: 'dd', quontity: 8, distance: 6000},
-				{date: '10.01.2022', name: 'ff', quontity: 6, distance: 5000},
-				{date: '11.01.2022', name: 'cc', quontity: 2, distance: 8000},
-				{date: '12.01.2022', name: 'dd', quontity: 3, distance: 3400},
-				{date: '13.01.2022', name: 'dd', quontity: 4, distance: 4500},
-				{date: '14.01.2022', name: 'aa', quontity: 4, distance: 4500},
-				{date: '16.01.2022', name: 'ff', quontity: 1, distance: 6220},
-				{date: '17.01.2022', name: 'ff', quontity: 8, distance: 6000},
-				{date: '18.01.2022', name: 'ff', quontity: 6, distance: 5000},
-				{date: '19.01.2022', name: 'cc', quontity: 2, distance: 8000},
-				{date: '20.01.2022', name: 'dd', quontity: 3, distance: 3400},
-				{date: '21.01.2022', name: 'gg', quontity: 4, distance: 4500},
-				{date: '22.01.2022', name: 'gg', quontity: 8, distance: 6000},
-				{date: '23.01.2022', name: 'gg', quontity: 3, distance: 4500},
-				{date: '24.01.2022', name: 'gg', quontity: 3, distance: 6220},
-				{date: '25.01.2022', name: 'gg', quontity: 4, distance: 6220},
-				{date: '26.01.2022', name: 'ff', quontity: 4, distance: 6220},
-				{date: '27.01.2022', name: 'rr', quontity: 7, distance: 6220},
-				{date: '28.01.2022', name: 'rr', quontity: 2, distance: 6220},
-				{date: '29.01.2022', name: 'aa', quontity: 6, distance: 6220},
-				{date: '30.01.2022', name: 'aa', quontity: 5, distance: 6220},
-				{date: '31.01.2022', name: 'asdfgh', quontity: 3, distance: 6220},
+				{date: '1.01.2022', name: 'AA', amount: 8, distance: 6000},
+				{date: '2.01.2022', name: 'aa', amount: 6, distance: 5000},
+				{date: '3.01.2022', name: 'Ab', amount: 2, distance: 8000},
+				{date: '4.01.2022', name: 'ab', amount: 3, distance: 3400},
+				{date: '5.01.2022', name: 'BB', amount: 4, distance: 4500},
+				{date: '6.01.2022', name: 'bb', amount: 8, distance: 6000},
+				{date: '7.01.2022', name: 'bA', amount: 4, distance: 4500},
+				{date: '8.01.2022', name: 'cc', amount: 1, distance: 6220},
+				{date: '9.01.2022', name: 'dd', amount: 8, distance: 6000},
+				{date: '10.01.2022', name: 'ff', amount: 6, distance: 5000},
+				{date: '11.01.2022', name: 'cc', amount: 2, distance: 8000},
+				{date: '12.01.2022', name: 'dd', amount: 3, distance: 3400},
+				{date: '13.01.2022', name: 'dd', amount: 4, distance: 4500},
+				{date: '14.01.2022', name: 'aa', amount: 4, distance: 4500},
+				{date: '15.01.2022', name: 'ff', amount: 1, distance: 6220},
+				{date: '16.01.2022', name: 'ff', amount: 1, distance: 6220},
+				{date: '17.01.2022', name: 'ff', amount: 8, distance: 6000},
+				{date: '18.01.2022', name: 'ff', amount: 6, distance: 5000},
+				{date: '19.01.2022', name: 'cc', amount: 2, distance: 8000},
+				{date: '20.01.2022', name: 'dd', amount: 3, distance: 3400},
+				{date: '21.01.2022', name: 'gg', amount: 4, distance: 4500},
+				{date: '22.01.2022', name: 'gg', amount: 8, distance: 6000},
+				{date: '23.01.2022', name: 'gg', amount: 3, distance: 4500},
+				{date: '24.01.2022', name: 'gg', amount: 3, distance: 6220},
+				{date: '25.01.2022', name: 'gg', amount: 4, distance: 6220},
+				{date: '26.01.2022', name: 'ff', amount: 4, distance: 6220},
+				{date: '27.01.2022', name: 'rr', amount: 7, distance: 6220},
+				{date: '28.01.2022', name: 'rr', amount: 2, distance: 6220},
+				{date: '29.01.2022', name: 'aa', amount: 6, distance: 6220},
+				{date: '30.01.2022', name: 'aa', amount: 5, distance: 6220},
+				{date: '31.01.2022', name: 'asdfgh', amount: 3, distance: 6220},
 			],
 		}
 	},
@@ -94,50 +95,44 @@ export default {
 	},
 	computed:{
 		part() {
-			let part= [];
-			for (let i = this.pagin; i < this.pagin + 10; i++ ) {
-				if (this.resultFilter[i]){
-					part.push(this.resultFilter[i]);
-				};
-			};
-			return part;
+			return this.resultFilter.slice(this.offset, this.offset + 10);
 		},
 	},
 
 	methods: {
 		back(){
-			if (this.pagin >= 10) {
-				this.pagin -= 10;
+			if (this.offset >= 10) {
+				this.offset -= 10;
 			};
 		},
 		next(){
-			if (this.pagin < this.resultFilter.length - 10) {
-				this.pagin += 10;
+			if (this.offset < this.resultFilter.length - 10) {
+				this.offset += 10;
 			};
 		},
 		applyFilter(){
-			if( this.valueToFilter === ' ' || this.param === ' ' || this.op === ' '){
+			if( this.valueToFilter === '' || this.param === '' || this.op === ''){
 				return alert('Фильтр не заполнен');
 			};
 			if (this.op === '<'){
-				this.resultFilter = this.schedule.filter(item => +item[this.param] < +this.valueToFilter);
+				this.resultFilter = this.schedule.filter(item => item[this.param].toString() < this.valueToFilter);
 			} else if (this.op === '='){
-				this.resultFilter = this.schedule.filter(item => +item[this.param] === +this.valueToFilter);
+				this.resultFilter = this.schedule.filter(item => item[this.param].toString() === this.valueToFilter);
 			} else if (this.op === '>'){
-				this.resultFilter = this.schedule.filter(item =>+item[this.param] > +this.valueToFilter);
+				this.resultFilter = this.schedule.filter(item => item[this.param].toString() > this.valueToFilter);
 			}else if (this.op === 'contains'){
-				this.resultFilter = this.schedule.filter(item => (item[this.param]).toString().includes(this.valueToFilter));
+				this.resultFilter = this.schedule.filter(item => item[this.param].toString().includes(this.valueToFilter));
 			};
 			this.res = true;
-			this.pagin = 0;
+			this.offset = 0;
 		},
 
 		reset(){
-			this.resultFilter = this.schedule.filter(item => true);
+			this.resultFilter = this.schedule;
 			this.res = false;
-			this.valueToFilter = ' ';
-			this.param = ' ';
-			this.op = ' ';
+			this.valueToFilter = '';
+			this.param = '';
+			this.op = '';
 		},
 	}
 }
@@ -148,7 +143,6 @@ export default {
 .table{
 	height: 500px;
 }
-
 .table__wrap{
 	width: 800px;
 	display: flex;
